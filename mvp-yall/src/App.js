fe2c2205f263dbda621e6872fef7b00eaa64eeff
Vince Garcia <vinceryangarcia@gmail.com>
2021-02-09 15:42:10 -0800

    MVP YALL

--- mvp-yall/src/App.js
+++ mvp-yall/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    user:[],
+    followers: [],
+  }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/vinceryangarcia')
+      .then(res => {
+        this.setState({
+          user: res.data,
+        })
+      })
+      .catch( err => console.log( err ) )
+
+    axios.get('https://api.github.com/users/vinceryangarcia/followers')
+      .then( res => {
+        console.log(res.data)
+        this.setState({
+          followers: res.data,
+        })
+      })
+      .catch( err => console.log( err ) )
+  }
+
+  render(){
+    return(
+      <div>
+        <div>
+          <img src={ this.state.user.avatar_url }/>
+          <br></br>
+          <a href={ this.state.user.html_url }>{ this.state.user.login }</a>
+          Minions: { this.state.user.followers }
+        </div>
+
+        <div>
+   
+              {
+                this.state.followers.map( item => {
+                    return <div>
+                      <img src={item.avatar_url}/>
+                      <a href={item.html_url}>{item.login}</a>
+                    </div>
+                })
+              }
+          
+        </div>
+       
+
+      </div>
+    )
+  }
 }
 
 export default App;
